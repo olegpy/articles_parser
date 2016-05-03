3b0a371bc9ac3eb6ae706d54e0f769b3ab23795e
oleg <olegkriklivets@gmail.com>
2016-05-03 22:20:41 +0300

    save some changes

--- app_article/views.py
+++ app_article/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 import xml.etree.ElementTree as ET
-from httplib import BadStatusLine
 import urllib2
-import urllib
 import json
 
 
@@ -12,33 +10,6 @@ def parser(request):
 
     url_nbu = "https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5"
     url_privat = "https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=3"
-    # url = "http://export.yandex.ru/weather-ng/forecasts/34300.xml"
-    url = "https://www.gismeteo.ru/city/legacy/4368/"
-    url2 = "http://informer.gismeteo.ua/xml/33345_1.xml"
-
-    # file = open('export_weather.xml', 'w')
-    # file.write(urllib.urlopen(url2).read())
-    # file.close()
-
-    try:
-        s = urllib2.urlopen(url2)
-        contents = s.read()
-        file = open('export_weather.xml', 'w')
-        file.write(contents)
-        file.close()
-    # do something with page
-    except BadStatusLine:
-        print "could not fetch %s" % url2
-
-    # tree = ET.parse('export_weather.xml')
-    # print tree.getroot()
-    # root = tree.getroot()
-
-    # for r in root.iter('fact'):
-    #     print r
-    # lists.append(r.attrib)
-
-    # print root
 
     nbu_xml = pars_xml(url_nbu, 'export_nbu.xml')
     nbu_privat = pars_xml(url_privat, 'export_nbu.xml')
